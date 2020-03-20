11d489987bd0ffb1ca85b2d933cf825729cc2de1
ashub17 <ashubbinasad@gmail.com>
2020-03-20 21:04:38 +0600

    dynamic menus

--- app/Http/Controllers/crudController.php
+++ app/Http/Controllers/crudController.php
@@ -14,9 +14,20 @@ class crudController extends Controller
         print_r($data);
         $table=decrypt($data['table']);
         unset($data['table']);
+        if(!empty($data['image'])){
+            if(Input::hasFile('image')){
+              $data['image'] = $this->upload($data['image'],$table);
+            }
+        }
     	$data['created_at'] = date('Y-m-d H:i:s');
     	DB::table($table)->insert($data);
     	session::flash('message','Data inserted successfully!!!');
         return redirect()->back()->with('message','New data successfully inserted');
     }
+    private function upload($image,$table){
+        $name = $image->getClientOriginalName();
+        $newName = date('ymdgis').$name;
+        $image->move(public_path().'/'.$table,$newName);
+        return $newName;
+    } 
 }
